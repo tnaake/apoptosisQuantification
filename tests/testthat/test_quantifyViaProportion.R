5f18117b3c926a3a6104eb65fd9cb0e9abc0ce21
tnaake <thomasnaake@gmx.de>
2022-02-18 15:04:06 +0100

    Update test_quantifyViaProportion.R

--- tests/testthat/test_quantifyViaProportion.R
+++ tests/testthat/test_quantifyViaProportion.R
@@ -16,30 +16,30 @@ rownames(prot)[inds_mito] <- prot_mito
 ## function calculateProportionOfMitochondrialProteins
 test_that("calculateProportionOfMitochondrialProteins works.", {
     
-    prop <- calculateProportionOfMitochondrialProteins(prot = prot, 
+    prop <- calculateProportionOfMitochondrialProteins(values = prot, 
         id = "Symbol")
     expect_equal(length(prop), 10)
     expect_equal(names(prop), colnames(prot))
     expect_equal(as.vector(prop), 
         c(8.248111, 9.344613, 10.819496, 5.956062, 11.959543, 10.370720,
             7.144944, 10.102232, 11.072084, 9.893684), tolerance = 1e-06)
-    prop_foo <- calculateProportionOfMitochondrialProteins(prot = prot, 
+    prop_foo <- calculateProportionOfMitochondrialProteins(values = prot, 
         id = "Ensembl")
     expect_equal(length(prop), 10)
     expect_equal(names(prop), colnames(prot))
     expect_equal(as.vector(prop_foo), rep(0, 10))
-    expect_error(calculateProportionOfMitochondrialProteins(prot = 1:10),
+    expect_error(calculateProportionOfMitochondrialProteins(values = 1:10),
         "non-character argument")
     mat_foo <- matrix(1:100, ncol = 10)
-    expect_error(calculateProportionOfMitochondrialProteins(prot = mat_foo),
+    expect_error(calculateProportionOfMitochondrialProteins(values = mat_foo),
         "non-character argument")
-    expect_error(calculateProportionOfMitochondrialProteins(prot = prot, 
+    expect_error(calculateProportionOfMitochondrialProteins(values = prot, 
         id = "foo"), "'arg' should be one of ")
 })
 
 ## function plotProportionOfMitochondrialProteins
 test_that("plotProportionOfMitochondrialProteins works.", {
-    prop <- calculateProportionOfMitochondrialProteins(prot = prot, 
+    prop <- calculateProportionOfMitochondrialProteins(values = prot, 
         id = "Symbol")
     gg <- plotProportionOfMitochondrialProteins(proportion = prop)
     expect_is(gg, "gg")
